f4cc52f51e0e1e1bc9cede6a220f0226c997dada
muhilham98 <61240129+muhilham98@users.noreply.github.com>
2020-08-03 11:00:34 +0700

    update tampilan admin

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        return view('pages.admin.dashboard');
+        $users = DB::table('users')->count();
+        return view('pages.admin.dashboard',[
+                'users' => $users
+        ]);
     }
 }
